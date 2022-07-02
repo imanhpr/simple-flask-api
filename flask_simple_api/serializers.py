5411f57e164310a06a908a5d87a385f17118baab
ImanHpr <imanhpr1999@gmail.com>
2022-07-02 15:44:37 +0430

    add: add parser

--- flask_simple_api/serializers.py
+++ flask_simple_api/serializers.py
@@ -1,5 +1,7 @@
 from flask_restful import fields
+from flask_restful.reqparse import RequestParser
 
+############################################ Ouput Data ############################################
 _quote_fields = {
     "id": fields.Integer,
     "text": fields.String,
@@ -16,3 +18,9 @@ celebrity_fields = _celebrity_fields | {
 quote_fields = _quote_fields | {
     "celebrity": fields.Nested(_celebrity_fields),
 }
+
+####################################################################################################
+############################################## Parser ##############################################
+celebrity_parser = RequestParser(bundle_errors=True)
+celebrity_parser.add_argument("name", required=True, help="Name can not be blank!")
+####################################################################################################
