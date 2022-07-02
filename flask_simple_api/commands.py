7799aec0fe39e6abc93a44b1879d898eb7c96a80
ImanHpr <imanhpr1999@gmail.com>
2022-07-02 07:43:16 +0430

    add createtables command

--- flask_simple_api/commands.py
+++ flask_simple_api/commands.py
@@ -1,11 +1,18 @@
-from flask import Blueprint
-
-from .loggerfactory import make_log_dir
+from flask import Blueprint, current_app
+from .constant import DB
+from .loggerfactory import logger_factory, make_log_dir
 
 commands_bl = Blueprint("commands", __name__)
 
+logger = logger_factory(__name__)
 
 @commands_bl.cli.command("logdir")
 def log_dir_command():
     """Create the logs directory"""
     make_log_dir()
+
+@commands_bl.cli.command("createtables")
+def create_tables():
+    """Creating models in database"""
+    logger.info("Tables has been created.")
+    DB.create_all()
