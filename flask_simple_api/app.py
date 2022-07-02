bf00c7b7a39665bf5ca09bfb4421182229827d8e
ImanHpr <imanhpr1999@gmail.com>
2022-07-02 07:43:07 +0430

    Add postgres

--- flask_simple_api/app.py
+++ flask_simple_api/app.py
@@ -3,15 +3,21 @@ from flask import Flask
 
 def create_app() -> Flask:
     app = Flask(__name__)
-
+    app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ] = "postgresql://postgres:testpass@postgres_db/api_db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
     from .commands import commands_bl
+    from .models import models_bl
     from .urls import urls_bl
 
+    app.register_blueprint(models_bl)
     app.register_blueprint(commands_bl)
     app.register_blueprint(urls_bl)
 
-    from .constant import API
+    from .constant import API, DB
 
+    DB.init_app(app)
     API.init_app(app)
 
     return app
